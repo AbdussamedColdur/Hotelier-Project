3b0271e186d380a795cd1ba9e3f7b537a3331d0d
AbdussamedColdur <121455972+AbdussamedColdur@users.noreply.github.com>
2025-01-03 16:03:25 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=Samet; initial catalog=ApiDb; integrated security=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Service { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -0,0 +1,47 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.DataAccessLayer.Concrete; // Context class'ýný buraya ekledik
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
+builder.Services.AddControllers();
+
+// Swagger/OpenAPI yapýlandýrmasý
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
